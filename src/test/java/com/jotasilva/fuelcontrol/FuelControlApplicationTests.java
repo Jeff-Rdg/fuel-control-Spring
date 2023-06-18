b6f6a2cdee8ef553946a34276a43240b160f7699
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-06-18 14:51:48 -0300

    Create project

---
+++ src/test/java/com/jotasilva/fuelcontrol/FuelControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotasilva.fuelcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
