9f83c1d8b5a17366cca14de2f1690811c89b8160
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-06-18 14:54:42 -0300

    Create VehicleType enum

---
+++ src/main/java/com/jotasilva/fuelcontrol/entities/enums/VehicleType.java
@@ -0,0 +1,14 @@
+package com.jotasilva.fuelcontrol.entities.enums;
+
+public enum VehicleType {
+    MOTORGRADER(1),
+    ROADROLLER(2),
+    TIRETRACTOR(3),
+    PIPETRUCK(4),
+    BUCKETTRUCK(5),
+    BACKHOELOADER(6),
+    MIXED(7);
+
+    VehicleType(int number) {
+    }
+}
