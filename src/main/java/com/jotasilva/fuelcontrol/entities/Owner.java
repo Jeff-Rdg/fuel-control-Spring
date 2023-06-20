2ff39bf923e8426e5e0c11f3e7cdc3d1a7788d3a
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-06-19 22:03:31 -0300

    refactore Owner entity

--- src/main/java/com/jotasilva/fuelcontrol/entities/Owner.java
+++ src/main/java/com/jotasilva/fuelcontrol/entities/Owner.java
@@ -1,11 +1,23 @@
 package com.jotasilva.fuelcontrol.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Owner {
     private UUID id;
     private String cnpj;
     private String corporateName;
+
+    //relations
+    private List<Vehicle> vehicles = new ArrayList<>();
+
+    //region constructors
+    public Owner() {
+
+    }
+    //endregion
 
     //region Getters and Setters
 
@@ -31,6 +43,27 @@ public class Owner {
 
     public void setCorporateName(String corporateName) {
         this.corporateName = corporateName;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+    //endregion
+
+    //region equals and hashcode
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Owner owner = (Owner) o;
+        return Objects.equals(id, owner.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
     //endregion
 }
