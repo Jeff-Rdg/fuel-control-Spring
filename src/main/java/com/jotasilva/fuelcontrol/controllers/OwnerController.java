97a432bf834b4422e9e477fdd08ae8d1b1401034
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-06-23 21:15:30 -0300

    create findAll OwnerController

--- src/main/java/com/jotasilva/fuelcontrol/controllers/OwnerController.java
+++ src/main/java/com/jotasilva/fuelcontrol/controllers/OwnerController.java
@@ -1,7 +1,25 @@
 package com.jotasilva.fuelcontrol.controllers;
 
+import com.jotasilva.fuelcontrol.entities.Owner;
+import com.jotasilva.fuelcontrol.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/owner")
 public class OwnerController {
+    @Autowired
+    private OwnerService service;
+
+    @GetMapping
+    public ResponseEntity<List<Owner>> findAll(){
+        List<Owner> owners = service.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(owners);
+    }
 }
